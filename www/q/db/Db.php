104c6078d4c5ee2ac7940990fd9d3c8332465147
mike chin <chin.michael@gmail.com>
2015-08-25 15:25:39 -0600

    allow access to endpoint from frontend.

--- www/q/db/Db.php
+++ www/q/db/Db.php
@@ -7,15 +7,23 @@ class Db {
 
 	function __construct($user, $pass) {
 		try {
-			$this->db = new PDO( 'mysql:host=localhost; dbname=stx_core', $user, $pass );
+			$this->db = new PDO( "mysql:host=localhost; dbname=stx_core", $user, $pass );
 			$this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		}
 		catch(PDOException $e) {
-			echo 'error: ' . $e->getMessage();
+			header("HTTP/1.1 401 Unauthorized");
+			die();
 		}
 	}
 
 	public function getUserData($id) {
+		header("Access-Control-Allow-Origin: *");
+		header("Access-Control-Allow-Headers: Content-Type");
+		header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+		header("Access-Control-Allow-Credentials: true");
+		header("Access-Control-Allow-Max-Age: 86400");
+		header("Content-Type: application/json");
+
 		$q = $this->db->prepare("SELECT firstname, lastname FROM users WHERE usrId = :id");
 		$q->setFetchMode(PDO::FETCH_ASSOC);
 		$q->bindParam(':id', $id);
