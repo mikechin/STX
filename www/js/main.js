7054bafc7e67c9ba4e7d4f1ebb64829a09790d25
mike chin <chin.michael@gmail.com>
2015-09-01 13:28:56 -0600

    remove root stub, redirect to scan.

--- www/js/main.js
+++ www/js/main.js
@@ -2,9 +2,6 @@ var stx = angular.module('stx', ['ngRoute', 'ngCookies']);
 
 stx.config(['$routeProvider', function($routeProvider) {
 	$routeProvider.
-		when('/', {
-			templateUrl: 'views/main.html'
-		}).
 		when('/scan', {
 			controller: 'ScanController',
 			templateUrl: 'views/scan.html'
@@ -13,6 +10,6 @@ stx.config(['$routeProvider', function($routeProvider) {
 			templateUrl: 'views/customers.html'
 		}).
 		otherwise({
-			redirectTo: '/'
+			redirectTo: '/scan'
 		});
 }]);
