d58d33eba82f789ce19b1a83a35841b9ecb53ba4
mike chin <chin.michael@gmail.com>
2015-12-18 02:18:19 -0700

    search issuers by name.

--- www/js/controllers/IssuersController.js
+++ www/js/controllers/IssuersController.js
@@ -1,6 +1,49 @@
 stx.controller('IssuersController', ['$scope', '$http', function($scope, $http) {
 	'use strict';
 
+	// **************************************************
+	// private.
+	//
+	//
+	// **************************************************
+
+	// **************************************************
+	// public.
+	//
+	//
+	// **************************************************
+	$scope.issuer = {
+		id: '',
+		name: '',
+		search: false
+	};
+
+	$scope.issuers = [];
+
 	$scope.issuersSearch = function() {
+		if($scope.issuer.name === '') {
+			$scope.issuersForm.name.$invalid = true;
+			$scope.issuersForm.name.$dirty = true;
+
+			return;
+		}
+
+		var url = 'http://stx.localhost:8888/q/issuers/' + $scope.issuer.name;
+		$http({
+			method: 'GET',
+			url: url,
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			}
+		}).
+		success(function(data, status, headers, config) {
+			console.log('success.');
+			$scope.issuers = data.issuers;
+			$scope.issuer.search = true;
+		}).
+		error(function(data, status, headers, config) {
+			console.log('error.');
+		});
 	};
 }]);
