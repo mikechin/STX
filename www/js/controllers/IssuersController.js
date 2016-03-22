7c6db5da4fc915214eda54033b80f43ae2b7b893
mike chin <chin.michael@gmail.com>
2016-03-22 03:36:32 -0600

    add input box and logic for search by account.

--- www/js/controllers/IssuersController.js
+++ www/js/controllers/IssuersController.js
@@ -15,20 +15,47 @@ stx.controller('IssuersController', ['$scope', '$http', function($scope, $http) 
 	$scope.issuer = {
 		id: '',
 		name: '',
+		account: '',
 		search: false
 	};
 
 	$scope.issuers = [];
 
+	$scope.cleanInput = function(input) {
+		$scope.issuer[input] = '';
+		$scope.issuersForm[input].$dirty = false;
+	};
+
 	$scope.issuersSearch = function() {
-		if($scope.issuer.name === '') {
-			$scope.issuersForm.name.$invalid = true;
-			$scope.issuersForm.name.$dirty = true;
+		if(!$scope.issuer.name && !$scope.issuer.account) {
+			if(!$scope.issuer.name) {
+				$scope.issuersForm.name.$invalid = true;
+				$scope.issuersForm.name.$dirty = true;
+			}
+
+			if(!$scope.issuer.account) {
+				$scope.issuersForm.account.$invalid = true;
+				$scope.issuersForm.account.$dirty = true;
+			}
 
 			return;
 		}
+		else {
+			$scope.issuersForm.name.$dirty = false;
+			$scope.issuersForm.account.$dirty = false;
+		}
 
-		var url = 'http://stx.localhost:8888/q/issuers/' + $scope.issuer.name;
+		var url = 'http://stx.localhost:8888/q/issuers/';
+		if($scope.issuer.name !== '' && $scope.issuer.account !== '') {
+			url += $scope.issuer.name + '/' + $scope.issuer.account;
+		}
+		else if($scope.issuer.name !== '') {
+			url += $scope.issuer.name;
+		}
+		else {
+			url += 'acct/' + $scope.issuer.account;
+		}
+
 		$http({
 			method: 'GET',
 			url: url,
@@ -37,7 +64,7 @@ stx.controller('IssuersController', ['$scope', '$http', function($scope, $http) 
 				'Content-Type': 'application/json'
 			}
 		}).
-		success(function(data, status, headers, config) {
+			success(function(data, status, headers, config) {
 			console.log('success.');
 			$scope.issuers = data.issuers;
 			$scope.issuer.search = true;
