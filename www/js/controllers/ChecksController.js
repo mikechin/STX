c30f78c1bd7ff1eff7b680047fb8ca67787139c0
michael chin <chin.michael@gmail.com>
2017-02-25 00:03:43 -0700

    add checks controller that list previous 20 checks and searches by check number.

---
+++ www/js/controllers/ChecksController.js
@@ -0,0 +1,74 @@
+stx.controller('ChecksController', ['$scope', '$http', function($scope, $http) {
+	'use strict';
+
+	// **************************************************
+	// private.
+	//
+	//
+	// **************************************************
+	function init() {
+		var url = 'http://stx.localhost:8888/q/checks';
+		$http({
+			method: 'GET',
+			url: url,
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			}
+		}).
+		success(function(data, status, headers, config) {
+			console.log('success.', data);
+			$scope.checks = data.checks;
+			if($scope.checks.length > 0) {
+				$scope.show = true;
+			}
+		}).
+		error(function(data, status, headers, config) {
+			console.log('error.');
+		});
+	}
+
+  init();
+
+	// **************************************************
+	// public.
+	//
+	//
+	// **************************************************
+	$scope.checks = [];
+	$scope.show   = false;
+	$scope.search = {
+		number: ''
+	};
+
+	$scope.numberSearch = function(number) {
+		if(!$scope.search.number || $scope.search.number === '') {
+			if($scope.search.number === '') {
+				$scope.checkForm.number.$invalid = true;
+				$scope.checkForm.number.$dirty = true;
+			}
+
+			return;
+		}
+
+		var url = 'http://stx.localhost:8888/q/checks/' + $scope.search.number;
+		$http({
+			method: 'GET',
+			url: url,
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json'
+			}
+		}).
+		success(function(data, status, headers, config) {
+			console.log('success.', data);
+			$scope.checks = data.checks;
+			if($scope.checks.length > 0) {
+				$scope.show = true;
+			}
+		}).
+		error(function(data, status, headers, config) {
+			console.log('error.');
+		});
+	};
+}]);
